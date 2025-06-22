6254f10769b4f320198cd76e6030802b4b1619d5
Lucas Chiaratti <chiaratti@gmail.com>
2025-06-22 07:57:30 -0300

    2

---
+++ outputs.tf
@@ -0,0 +1,28 @@
+
+output "next_steps" {
+  value = <<EOT
+🎉 Cluster AKS criado com sucesso!
+
+👉 Para acessar seu cluster, execute o seguinte comando:
+
+az aks get-credentials --resource-group ${var.resource_group_name} --name ${var.aks_cluster_name}
+
+🚀 Ingress Controller instalado com sucesso!
+
+✅ Acesse o IP fixo criado:
+http://${azurerm_public_ip.ingress_ip.ip_address}
+
+📄 Você verá uma mensagem "404 Not Found" do NGINX — isso é esperado!
+
+➡️ Prossiga para o Lab 03 para criar uma aplicação + Ingress Resource.
+
+EOT
+}
+
+output "resource_group_name" {
+  value = var.resource_group_name
+}
+
+output "aks_cluster_name" {
+  value = var.aks_cluster_name
+}
